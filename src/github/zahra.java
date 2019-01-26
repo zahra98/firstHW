c3a7b9cb2fcec8441c0fa197607b47323245895f
Shayma AbuZahra <shayma.abuzahra@radixtechs.com>
2019-01-26 18:47:14 +0200

    family name added....

---
+++ src/github/zahra.java
@@ -0,0 +1,11 @@
+package github;
+import java.util.*;
+public class zahra {
+public static void printName(){
+	System.out.println("zahra ayed");
+}
+
+public static void main (String []args) {
+	zahra.printName();
+}
+}
